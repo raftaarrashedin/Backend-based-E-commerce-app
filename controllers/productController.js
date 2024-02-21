84d10ed11054245b7af868748c61c1c8eee6a41d
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-21 23:41:09 +0530

    Complete Updated product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const {productValidation, validationResult} = require('express-validator')
 const db = require('../config/dbConnection')
 
-const product = (req,res) => {
+const createProduct = (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors : errors.array()})
@@ -10,7 +10,7 @@ const product = (req,res) => {
     const productData = {
         product_name : req.body.product_name,
         product_detail : req.body.product_detail,
-        product_category : req.body.	product_category,
+        product_category : req.body.product_category,
         product_price : req.body.product_price,
         product_owner : req.body.product_owner
     };
@@ -26,8 +26,40 @@ const product = (req,res) => {
     
 }
 
+
+// Functions to handle Update product
+
+const updateProduct = (req,res) => {
+    
+    const productId = req.params.id;
+
+    const updateProductData = {
+        product_name : req.body.product_name,
+        product_detail : req.body.product_detail,
+        product_category : req.body.product_category,
+        product_price : req.body.product_price,
+        product_owner : req.body.product_owner 
+    };
+    db.query('UPDATE product SET ? WHERE id = ?',[updateProductData,productId], (errors,results) => {
+
+        if(errors) {
+            console.error('Error Updating Product',errors);
+            return res.status(405).json({errors: 'Failed to update product!'})
+        }
+        console.log(" Product Update Successfull!")
+        return res.status(200).json({message : "Product Update Successfull!!!"})
+    })
+}
+
+
+
+
+
+
+
+
 module.exports = {
-    product
+    createProduct
 }
 // db.query(
     //     `INSERT INTO product(product_name,product_detail,product_category,product_price,product_owner) VALUES(
